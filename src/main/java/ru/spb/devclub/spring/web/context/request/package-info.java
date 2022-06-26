0f3694d8e19d344bf0f8a7474b38602ae2d84201
Grig Alex <alexengrigdev@gmail.com>
2022-06-27 02:01:45 +0300

    Create main package-info.java

---
+++ src/main/java/ru/spb/devclub/spring/web/context/request/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Delegating
+ * <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/context/request/RequestContextHolder.html">
+ * RequestContextHolder
+ * </a>
+ * like
+ * <a href="https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/core/context/SecurityContextHolder.html">
+ * SecurityContextHolder
+ * </a>
+ * in
+ * <a href="https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/concurrent/package-summary.html">
+ * org.springframework.security.concurrent
+ * </a>
+ * and
+ * <a href="https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/task/package-summary.html">
+ * org.springframework.security.task
+ * </a>
+ * .
+ *
+ * @since 1.0
+ */
+package ru.spb.devclub.spring.web.context.request;
